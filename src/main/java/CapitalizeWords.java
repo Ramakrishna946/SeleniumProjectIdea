f237adfb8b4deae6aa7206f6b1d1ffc2a1224e32
Ramakrishna <chkrishna0070@gmail.com>
2025-11-15 12:22:02 +0530

    capital

---
+++ src/main/java/CapitalizeWords.java
@@ -0,0 +1,17 @@
+public class CapitalizeWords {
+
+    public static void main(String[] args) {
+        String input="nothing is impossible";
+        String[] words=input.split(" ");
+        StringBuilder sb= new StringBuilder();
+        for(String word : words) {
+            if(!word.isEmpty()) {
+                sb.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1)).append(" ");
+
+            }
+        }
+        String result= sb.toString().trim();
+        System.out.println(result);
+    }
+
+}
