64c7e3e012dd2e886c25494f534905ea1832027c
Ramakrishna <chkrishna0070@gmail.com>
2025-11-16 06:31:12 +0530

    developer changes

---
+++ src/test/java/Testsample.java
@@ -0,0 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+
+public class Testsample {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.quit();
+    }
+}
